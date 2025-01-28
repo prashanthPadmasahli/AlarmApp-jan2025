e25fec8bee4c8c2f7f8d14190ac642d56a9e6c7a
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-28 16:52:42 +0530

    Initial Commit

---
+++ AlarmApp-jan2025/AlarmApp_jan2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp_jan2025App.swift
+//  AlarmApp-jan2025
+//
+//  Created by mac on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp_jan2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmApp-jan2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlarmApp-jan2025
+//
+//  Created by mac on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
